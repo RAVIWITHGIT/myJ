862211908ea731354679ca92663952f575c4f316
RAVIWITHGIT <ravilakshakar336@gmail.com>
2024-09-06 19:34:08 +0530

    output jav filea

---
+++ variables_&_Data_Types/output_in_java/output.java
@@ -0,0 +1,61 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+/*  ************* boilerplate code of java
+public class output{
+    public static void main(String args[]){
+        
+    }
+}
+*/
+
+/* *********** how to print any thing using System.out.print() method 
+public class output{
+    public static void main(String args[]){
+        System.out.print("hello world");
+        System.out.print("hello ravi");
+        System.out.print("hello krishna");
+    }
+}
+*/
+
+/* *********** output in newline using two way
+1. using System.out.println() method
+2. using backslace n (\n)
+
+1. println() ******************************
+
+public class output{
+    public static void main(String args[]){
+        System.out.println("hello world");
+        System.out.println("hello ravi");
+        System.out.println("hello krishna");
+    }
+}
+
+
+2. backslace n (\n) ********************
+
+public class output{
+    public static void main(String args[]){
+        System.out.print("hello world\n");
+        System.out.print("hello ravi\n");
+        System.out.print("hello ram\n");
+    }
+}
+
+
+*/
+public class output{
+    public static void main(String args[]){
+        System.out.print("hello world\n");
+        System.out.print("hello\n ravi\n");
+        System.out.print("hello ram\n");
+    }
+}
+
