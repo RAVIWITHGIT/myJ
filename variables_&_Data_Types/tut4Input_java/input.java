3cbcc292c59970339cc0c3cb843d5e0efa383476
RAVIWITHGIT <ravilakshakar336@gmail.com>
2024-09-10 18:39:08 +0530

    input in java

---
+++ variables_&_Data_Types/tut4Input_java/input.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class input
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//*****for input string use next()
+		//String input = sc.next();
+		
+		//****** for input string with space use nextLine()
+		//String input= sc.nextLine();
+		
+		//****** for input int , use nextInt()
+		//int input = sc.nextInt();
+		
+		//****** for input float , use nextFloat()
+		//float input =sc.nextFloat();
+		
+		//****** for input float with large range , use nextDouble()
+		//double input = sc.nextDouble();
+		
+		//****** for input integer with large range , use nextLong()
+		//long input = sc.nextLong();
+		
+		//***** fir input boolean value , use nextBoolean()
+		// boolean input =sc.nextBoolean();
+		System.out.println(input); 
+	}
+}
