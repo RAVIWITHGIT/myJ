c6ed52a1e565e9c99890c5570b98d7766dff13d4
RAVIWITHGIT <ravilakshakar336@gmail.com>
2024-09-10 18:35:51 +0530

    data type in java

---
+++ variables_&_Data_Types/tut3Datatype_java/tut3.java
@@ -0,0 +1,38 @@
+/* ********************* data type ********************************
+two data type present in java
+1. primitive data type --> which already available in java , not need making 
+. byte
+. short
+. char
+. boolean
+. int
+. long
+. float
+. double
+
+2. non primitive data type --> which not exist in java .To use this you have to make these first
+. String
+. array 
+. class
+. Object
+. interface
+
+*/
+
+public class tut3{
+    public static void main(String args[]){
+        byte b=8;
+        char c ='a';
+        boolean bl =true;
+        float price = 10.5f;
+        //double
+        int number =10;
+        //long
+        short n=240;
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(bl);
+        System.out.println(price);
+        System.out.println(number);
+    }
+}
