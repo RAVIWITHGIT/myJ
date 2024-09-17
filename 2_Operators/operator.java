30f6ca560aedf07e30ed30a7f28e6f5031dfad6b
RAVIWITHGIT <ravilakshakar336@gmail.com>
2024-09-17 20:23:49 +0530

    Operator in java

---
+++ 2_Operators/operator.java
@@ -0,0 +1,22 @@
+/* *************************** Operator**************************
+Whenever we perform any operation, it has two major components
+1. operands
+2. operator 
+
+*/
+
+/* ************************** Arithmetic operator **************
+Arithmetic operator have two type
+1. Binary --> which have two operand 
+ +,-,*,/,%
+
+2. unary --> It requires one  operand to perform the operation.
+++ , --
+*/
+
+
+public class operator{
+    public static void main(String args[]){
+        System.out.println("hello World");
+    }
+}
