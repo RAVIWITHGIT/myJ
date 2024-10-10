1c1d256b152b88de9e162312df52fbbb274bcdc2
RAVIWITHGIT <ravilakshakar336@gmail.com>
2024-10-10 19:37:46 +0530

    update code

--- tut5_Patterns(part1)/Task.java
+++ tut5_Patterns(part1)/Task.java
@@ -31,6 +31,25 @@
 // }
 
 
+// public class Task{
+//     public static void main(String args[]){
+//         int range = 4;
+//         for(int i=1; i<=4; i++){
+//             for(int j=1; j<=5; j++){
+//                 if(i==1 || i==range || j==1 || j==5){
+//                     System.out.print("*");
+//                 }
+//                 else{
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+
+//         }
+//     }
+// }
+
+
 /* ****************************************************************
       *
     * *
