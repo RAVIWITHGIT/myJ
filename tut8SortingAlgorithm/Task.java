c6a2414e8d54659fa8475a93e42f065cf3c55c52
ProjectWithRavi <raviyt73@gmail.com>
2024-11-13 19:54:24 +0530

    update code

--- tut8SortingAlgorithm/Task.java
+++ tut8SortingAlgorithm/Task.java
@@ -1,3 +1,30 @@
+/* ************************************ bubble sort 
+// public class Task{
+//     public static void bubbleSort(int arr[]){
+//         for(int i=0; i<arr.length-1; i++){
+//             for(int j=0; j<arr.length-1-i; j++){ 
+//                 if(arr[j]>arr[j+1]){
+//                     int temp = arr[j];
+//                     arr[j] = arr[j+1];
+//                     arr[j+1] = temp;
+//                 }
+//             }
+            
+//         }
+
+//         for(int i=0; i<arr.length; i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+//     public static void main(String args[]){
+//         int arr[] = {3,6,2,1,8,7,4,5,3,1};
+//         bubbleSort(arr);
+//     }
+// }
+*/
+
+
+
 /* ******************** selection sort
 public class Task
 {
@@ -23,23 +50,30 @@ public class Task
 	}
 	
 }
+
 */
 
 
-/*  ************************************** bubbleSort 
-*/
+
+
+
+/* ************************************** insertionSort
+ */
+
+
 // public class Task{
-//     public static void bubbleSort(int arr[]){
-//         for(int i=0; i<arr.length-1; i++){
-//             for(int j=0; j<arr.length-1-i; j++){ 
-//                 if(arr[j]>arr[j+1]){
-//                     int temp = arr[j];
-//                     arr[j] = arr[j+1];
-//                     arr[j+1] = temp;
-//                 }
+//     public static void insertionSort(int arr[]){
+//         for(int i=1; i<arr.length;i++){
+//             int current = arr[i];
+//             int previous = i-1;
+//             while(previous>=0 && arr[previous]>current){
+//                 arr[previous+1] = arr[previous];
+//                 previous--;
 //             }
-            
+//             arr[previous+1] = current;
+
 //         }
+
 
 //         for(int i=0; i<arr.length; i++){
 //             System.out.print(arr[i]+" ");
@@ -47,6 +81,7 @@ public class Task
 //     }
 //     public static void main(String args[]){
 //         int arr[] = {3,6,2,1,8,7,4,5,3,1};
-//         bubbleSort(arr);
+//         insertionSort(arr);
+
 //     }
 // }
