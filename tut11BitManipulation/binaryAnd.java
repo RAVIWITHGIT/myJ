6ca4d5374244ca7f055871e9f7e576ccb2211cc6
RAVIWITHGIT <ravilakshakar336@gmail.com>
2024-12-17 19:49:10 +0530

    update code

---
+++ tut11BitManipulation/binaryAnd.java
@@ -0,0 +1,20 @@
+package tut11BitManipulation;
+
+/* ********************************************* binary And Operator ***********************************
+ 0 & 0 --> 0
+ 0 & 1 --> 0
+ 0 & 1 --> 0
+ 1 & 1 --> 1
+
+
+ if i give example of 5 & 2
+ 5 --> 101;
+ 2 -->  10;
+ result --> 101 & 10 ==> 000
+ */
+
+public class binaryAnd {
+    public static void main(String args[]) {
+        System.out.println(5 & 2);
+    }
+}
---
+++ tut11BitManipulation/binaryXOR.java
@@ -0,0 +1,20 @@
+package tut11BitManipulation;
+
+/* ********************************************* binary And Operator ***********************************
+ 0 ^ 0 --> 0
+ 0 ^ 1 --> 1
+ 0 ^ 1 --> 1
+ 1 ^ 1 --> 0
+
+
+ if i give example of 5 & 2
+ 5 --> 101;
+ 2 -->  10;
+ result --> 101 ^ 10 ==> 111
+ */
+
+public class binaryXOR {
+    public static void main(String args[]) {
+        System.out.println(5 ^ 2);
+    }
+}
