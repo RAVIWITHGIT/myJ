0ae7aa8416a1f5a5f2cc3b582594d93afe36a178
RAVIWITHGIT <ravilakshakar336@gmail.com>
2025-01-10 07:10:13 +0530

    update code

--- tut11BitManipulation/FirstCompliment.java
+++ tut11BitManipulation/FirstCompliment.java
@@ -1,14 +1,16 @@
 package tut11BitManipulation;
 
-/* *********************************************** One's compliment *******************************
+/* *********************************************** One's compliment/NOT Operator ************************
 1.if you find ones compliment of any number then use this symbol ~
 2.this symbol convert 0 to 1 and 1 to 0
 3. if one's compliment have 1 in left side then result in negative 
 
 here we find One's compliment of 5 
--:first 5 --> 00000101 then ~ convert --> 11111010; but in left side of one's compliment of 5 (11111010) is 1
--:so 11111010 convert to 00000101 and then add 1 = 00000110 
--: 00000110 is 6 , but get -6
+-:first 5 --> 00000101 then ~ convert --> 11111010; but in left side (also called MSB) of one's compliment of 5 (11111010) is 1
+-: so again get once's compliment of this binary number 
+-:so 11111010 convert to 00000101 by once's compliment(~) and then add 1 =00000101+1 -->  00000110 
+-: 00000110 is 6 , but get -6 because MSB OF once's compliment (111110101) of 5 is 1 .
+-: 1 indicate the value is negative 6 not positive 6 
 
 */
 
--- tut11BitManipulation/tut10PracticeQuestion.java
+++ tut11BitManipulation/tut10PracticeQuestion.java
@@ -5,9 +5,22 @@ public class tut10PracticeQuestion {
         return n ^ n;
     }
 
+    public static void swapValue(int f, int s) {
+        System.out.println("before swapping " + f + " " + s);
+        f = f ^ s;
+        s = f ^ s;
+        f = f ^ s;
+        System.out.println("after swapping " + f + " " + s);
+        System.out.println(~5);
+
+    }
+
     public static void main(String args[]) {
 
         // Question 1 :What is the value of x^x (^ xor operator) for any value of x
         System.out.println(solveOne(4));
+
+        // Question 2 :Swap two numbers without using any third variable
+        swapValue(5, 2);
     }
 }
