a15b2e6e86f04b076550428402b92dd04f812a20
RAVIWITHGIT <ravilakshakar336@gmail.com>
2025-01-12 07:10:41 +0530

    update code

--- tut12RecursionBasic/tut1DecreasingOrder.java
+++ tut12RecursionBasic/tut1DecreasingOrder.java
@@ -1,5 +1,29 @@
 
 public class tut1DecreasingOrder {
+    /*
+     * **************************recursion work in 3 step
+     * 1. first we will decide where this function stop , mean decide base case
+     * 2. what will this function do , mean Recursive Case
+     * 3. Function Call,
+     * 
+     * Analysis output ==>
+     * 1.when function call another function , then in memory create call stack
+     * 
+     * 2. call DecreasingOrder function in main function , which store in stack .
+     * a. after call first check n==1 , if true then print n and return
+     * b. if condition false then print n , and call inner function with n-1 value;
+     * 
+     * 3. inner function store in stack
+     * a. check n==1 , if true then print n and return
+     * b. if condition false then print n , and call inner function with n-1 value;
+     * 
+     * 4. when inner function store in stack with 1 value
+     * a. first check n==1 , if true then print n and return
+     * b. after return , remove from stack and other function also follow this step
+     * and remove from stack
+     * 
+     * 5. so call stack follow first in and last out (FILO)
+     */
     public static void DecreasingOrder(int n) {
         if (n == 1) {
             System.out.print(1 + " ");
---
+++ tut12RecursionBasic/tut2IncreasingOrder.java
@@ -0,0 +1,48 @@
+public class tut2IncreasingOrder {
+
+    /*
+     * *****************************************************************************
+     * ********
+     * 1. call IncreasingOrder function and give 5 integer value as argument
+     * 2. create a call stack in memory and function store in stack
+     * 3. after calling check if condition
+     * 4. if condition false then again call IncreasingOrder() with n-1 value
+     * 5. then again store in stack
+     * 6. check if condition
+     * 7. if condition false then again call IncreasingOrder() with n-1 value
+     * 8. This process will continue until it finds the base case.
+     * 9. after get base case , mean n==1 then if condition run and print n which is
+     * 1
+     * 10. after print n return from this function
+     * 
+     * 
+     * removing process in call stack
+     * 1. get n==1 , then print n===> 1 and return
+     * 2. so remove from stack
+     * 3. then reach IncreasingOrder(2) , which have store IncreasingOrder(n-1) , so
+     * IncreasingOrder(n-1) already finish , so run next line which is print
+     * statement . print n===> 2
+     * 4. after run all code of IncreasingOrder(2) , then remove from stack
+     * 5. then reach IncreasingOrder(3) , which have store IncreasingOrder(2) , so
+     * IncreasingOrder(2) already finish , so run next line which is print
+     * statement.print n===> 2
+     * 6. after run all code of IncreasingOrder(3) , then remove from stack
+     * 7. This process will continue until it reaches the bottom of the call stack
+     * 
+     * 
+     * 
+     */
+    public static void IncreasingOrder(int n) {
+        if (n == 1) {
+            System.out.print(n + " ");
+            return;
+        }
+        IncreasingOrder(n - 1);
+        System.out.print(n + " ");
+
+    }
+
+    public static void main(String args[]) {
+        IncreasingOrder(5);
+    }
+}
