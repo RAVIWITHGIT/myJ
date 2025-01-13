f540b2ff59bbcb6bef35af65fb92fa0d8c4f610e
RAVIWITHGIT <ravilakshakar336@gmail.com>
2025-01-13 12:48:21 +0530

    update code

--- tut12RecursionBasic/tut9PowerValue.java
+++ tut12RecursionBasic/tut9PowerValue.java
@@ -8,7 +8,25 @@ public class tut9PowerValue {
 
     }
 
+    public static int PowerValue2(int value, int power) {
+        if (power == 0) {
+            return 1;
+        }
+        int halfPower = PowerValue2(value, power / 2);
+        int halfPowerSquare = halfPower * halfPower;
+
+        if (power % 2 != 0) {
+            halfPowerSquare = 2 * halfPowerSquare;
+
+        }
+
+        return halfPowerSquare;
+
+    }
+
     public static void main(String args[]) {
         System.out.println(PowerValue(2, 5));
+        System.out.println(PowerValue2(2, 5));
+        System.out.println(1 % 2);
     }
 }
