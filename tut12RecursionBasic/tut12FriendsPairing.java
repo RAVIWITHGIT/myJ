0316fee26efac039622721ac0b9cf42d990c827f
RAVIWITHGIT <ravilakshakar336@gmail.com>
2025-01-18 07:57:01 +0530

    update code

---
+++ tut12RecursionBasic/tut12FriendsPairing.java
@@ -0,0 +1,12 @@
+
+/* ************************************* Friends pairing problem ************************************
+given n friends, each one can remain single or can be paired up with some other friend. Each friend can be paired only once. find out the total number of ways in which friends can remain single or can be paired up.
+ 
+ */
+
+public class tut12FriendsPairing {
+    public static void main(String args[]) {
+        System.out.println("good morning ravi");
+    }
+
+}
